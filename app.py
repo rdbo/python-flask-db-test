2cb56d0e3371fe4787154edcbb07add6c6df12d8
Rdbo <57117082+rdbo@users.noreply.github.com>
2021-01-06 15:14:45 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask("test")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+user_count = 0
 
 class UserInput(db.Model):
-    uid = db.Column("user_id", db.String(10), unique=True, nullable=False, primary_key=True)
+    uid = db.Column("user_id", db.String(10), unique=True, primary_key=True)
     username = db.Column(db.String(10), unique=True, nullable=False)
     password = db.Column(db.String(10), unique=True, nullable=False)
 
     def __init__(self, _username : str, _password : str):
+        global user_count
+        self.uid = user_count
         self.username = _username
         self.password = _password
+        user_count += 1
 
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index.html", methods=["GET", "POST"])
 def index():
+    global user_count
     if request.method == "POST":
         username = request.form["user"]
         password = request.form["pass"]
@@ -25,6 +30,7 @@ def index():
             data = UserInput(username, password)
             db.session.add(data)
             db.session.commit()
+        return redirect(url_for("index"))
     else:
         return render_template("index.html", user_list=UserInput.query.all())
 
